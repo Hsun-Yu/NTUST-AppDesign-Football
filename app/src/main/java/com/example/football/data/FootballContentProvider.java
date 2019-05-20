0b7855f2c626b1546697d98346d2959472cd2cc1
hsunyu <leoxz02leo@outlook.com>
2019-05-21 00:09:03 +0800

    1.Create project 2.Create ContentProvider

---
+++ app/src/main/java/com/example/football/data/FootballContentProvider.java
@@ -0,0 +1,111 @@
+package com.example.football.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import static com.example.football.data.FootballContract.FootballEntry.TABLE_NAME;
+
+
+public class FootballContentProvider extends ContentProvider {
+    // It's convention to use 100, 200, 300, etc for directories,
+    // and related ints (101, 102, ..) for items in that directory.
+    public static final int PLAYERS = 100;
+    public static final int PLAYER_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+
+    public static UriMatcher buildUriMatcher(){
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
+          All paths added to the UriMatcher have a corresponding int.
+          For each kind of uri you may want to access, add the corresponding match with addURI.
+          The two calls below add matches for the task directory and a single item by ID.
+         */
+        uriMatcher.addURI(FootballContract.AUTHORITY, FootballContract.PATH_PLAYERS, PLAYERS);
+        uriMatcher.addURI(FootballContract.AUTHORITY, FootballContract.PATH_PLAYERS + "/#", PLAYER_WITH_ID);
+
+        return uriMatcher;
+    }
+
+
+
+    private FootballDBHelper mFootballDbHelper;
+
+    public FootballContentProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // COMPLETED (1) Get access to the task database (to write new data to)
+        final SQLiteDatabase db = mFootballDbHelper.getWritableDatabase();
+
+        // COMPLETED (2) Write URI matching code to identify the match for the tasks directory
+        int match = sUriMatcher.match(uri);
+        Uri returnUri; // URI to be returned
+
+        switch (match) {
+            case PLAYERS:
+                // (3) Insert new values into the database
+                // Inserting values into tasks table
+                long id = db.insert(TABLE_NAME, null, values);
+                if ( id > 0 ) {
+                    returnUri = ContentUris.withAppendedId(FootballContract.FootballEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            // Set the value for the returnedUri and write the default case for unknown URI's
+            // Default case throws an UnsupportedOperationException
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Notify the resolver if the uri has been changed, and return the newly inserted URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return constructed uri (this points to the newly inserted row of data)
+        return returnUri;
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mFootballDbHelper = new FootballDBHelper(context);
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
