cb2c067f4282a03a9bf6e0b9791b37c0030ba95a
hsunyu <leoxz02leo@outlook.com>
2019-05-23 20:20:59 +0800

    Use preference fragment to make setting function with Play_Music

--- FootballPlayer/app/src/main/java/com/example/footballplayer/SettingFragment.java
+++ FootballPlayer/app/src/main/java/com/example/footballplayer/SettingFragment.java
@@ -1,4 +1,18 @@
 package com.example.footballplayer;
 
-public class SettingFragment {
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+public class SettingFragment extends PreferenceFragmentCompat {
+    // Create a class called SettingsFragment that extends PreferenceFragmentCompat
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        // In SettingsFragment's onCreatePreferences method add the preference file using the
+        // addPreferencesFromResource method
+
+        // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
 }
