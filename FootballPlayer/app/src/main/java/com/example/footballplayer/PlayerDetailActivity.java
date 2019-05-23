23300559a7c1c4a71ffd0d518c190d461ffb883d
hsunyu <leoxz02leo@outlook.com>
2019-05-24 01:40:50 +0800

    Show start on startBar

--- FootballPlayer/app/src/main/java/com/example/footballplayer/PlayerDetailActivity.java
+++ FootballPlayer/app/src/main/java/com/example/footballplayer/PlayerDetailActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class PlayerDetailActivity extends AppCompatActivity {
@@ -30,13 +31,14 @@ public class PlayerDetailActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        ((RatingBar)findViewById(R.id.start_detail_ratingbar)).setRating(playerStart);
         ((TextView)findViewById(R.id.player_team_detail_textview)).setText(playerTeam);
         ((TextView)findViewById(R.id.player_num_detail_textview)).setText("" + playerNum);
         if(playerStart == 1)
-            ((TextView)findViewById(R.id.player_start_detail_view)).setText("Not Good");
+            ((TextView)findViewById(R.id.player_start_detail_view)).setText("He is not a Good player");
         else if(playerStart == 2)
-            ((TextView)findViewById(R.id.player_start_detail_view)).setText("Good");
+            ((TextView)findViewById(R.id.player_start_detail_view)).setText("He is a good player");
         else if(playerStart == 3)
-            ((TextView)findViewById(R.id.player_start_detail_view)).setText("Very Good");
+            ((TextView)findViewById(R.id.player_start_detail_view)).setText("He is awesome!!!");
     }
 }
